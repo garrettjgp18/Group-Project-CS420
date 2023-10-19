e47f4aedaa76bb44c73a897c9a1c181f417c6bc2
Joseph Penton <josephpenton2@gmail.com>
2023-10-18 23:34:34 -0500

    Added Controller and skeleton of main view

---
+++ Precision Farming APp/src/Controller/LookandFeelController.java
@@ -0,0 +1,18 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class LookandFeelController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
