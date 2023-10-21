18e00943c1d3957597f799692596cf21d66b549f
Joseph Penton <josephpenton2@gmail.com>
2023-10-20 22:56:38 -0500

    Added more buttons to FXML Toyed with TreeItem commands to no avail. WIP

--- Precision Farming APp/src/Controller/LookandFeelController.java
+++ Precision Farming APp/src/Controller/LookandFeelController.java
@@ -1,12 +1,28 @@
 package Controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Group;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.layout.VBox;
 
 public class LookandFeelController {
+
+    @FXML
+    private Button addContainer;
+
+    @FXML
+    private Button addItem;
+
+    @FXML
+    private VBox componentButtons;
+
+    @FXML
+    private Button deleteObject;
 
     @FXML
     private TextField detailsHeight;
@@ -33,12 +49,23 @@ public class LookandFeelController {
     private Group farmGrid;
 
     @FXML
+    private TreeView<?> farmHierarchy;
+
+    @FXML
     private Button farmScan;
 
     @FXML
     private Button goToItem;
 
     @FXML
-    private AnchorPane pageAlerts;
+    private TextArea pageAlerts;
+
+
+
+
+    @FXML
+    void btnAddContainer(ActionEvent event) {
+
+    }
 
 }
--- Precision Farming APp/src/MenuView/App.java
+++ Precision Farming APp/src/MenuView/App.java
@@ -6,12 +6,14 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TreeItem;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 //add a comment to test pull from the working branch
 public class App extends Application {
 
     private Stage primaryStage;
+
 
     @Override
     public void start(Stage primaryStage) {
